5676df1128fe200902077d9b341550a41a800c9c
Sameer Ali <spiyarali17@gmail.com>
2017-08-17 16:41:57 +0500

    second commit

--- server/server.js
+++ server/server.js
@@ -6,6 +6,6 @@ var app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
-app.listen(3000,function(){
+app.listen(port,function(){
   console.log(`Server is up on ${port}`);
 })
