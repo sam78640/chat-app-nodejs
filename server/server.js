9b455c4d441b2a34c3239468fca9356efe84ed5e
Sameer Ali <spiyarali17@gmail.com>
2017-08-17 16:34:02 +0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const publicPath = path.join(__dirname,'../public');
+const express = require('express');
+
+var app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+
+app.listen(3000,function(){
+  console.log(`Server is up on ${port}`);
+})
